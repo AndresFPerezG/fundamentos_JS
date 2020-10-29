ddec58b793e3fd4b145771feb064b5263e2d0820
Andrés Pérez <andresfe52@gmail.com>
2020-10-28 22:05:35 -0500

    v3 poo

--- fundamentos_JS/POO_actualizado.js
+++ fundamentos_JS/POO_actualizado.js
@@ -4,8 +4,12 @@ class Persona {
         this.apellido = apellido
         this.altura = altura
     } 
-    saludar(){
-        console.log(`Hola, me llamo ${this.nombre} ${this.apellido}`)
+    saludar(fn){ // Función vaciá solo para validar la función de responder saludo
+        var { nombre, apellido } = this
+        console.log(`Hola, me llamo ${nombre} ${apellido}`)
+        if(fn){
+            fn(nombre, apellido)
+        }
     }
     soyAlto(){
         this.altura > 1.80 ? console.log(`${this.nombre} es alto/a`) : console.log(`${this.nombre} es bajo/a`)
@@ -16,11 +20,26 @@ class Desarrollador extends Persona{
     constructor(nombre, apellido, altura){
         super(nombre, apellido, altura)
     }
-    saludar(){
-        console.log(`Hola, me llamo ${this.nombre} ${this.apellido} y soy desarrollador/a`)
+    saludar(fn){
+        var { nombre, apellido } = this
+        console.log(`Hola, me llamo ${nombre} ${apellido} y soy desarrollador/a`)
+        if(fn){
+            fn(nombre, apellido, true) //true porque se valida que sea un desarrollador
+        }
+    }
+}
+
+function responderSaludo(nombre, apellido, esDev){
+    console.log(`Buen día ${nombre} ${apellido}`)
+    if (esDev){
+        console.log(`Ah que bueno que eres Desarrollador`)
     }
 }
 
 var andres = new Desarrollador('Andrés', 'Pérez', 1.81)
 var carlos = new Persona('Carlos', 'Gomez', 1.86)
 var margarita = new Persona('Margarita', 'Marín', 1.50)
+
+andres.saludar(responderSaludo)
+carlos.saludar(responderSaludo)
+margarita.saludar(responderSaludo)
